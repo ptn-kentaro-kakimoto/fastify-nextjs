b803d2dd72887877b8f13c8c03438a9b7bb67995
ptn-kentaro-kakimoto <ptn-kentaro.kakimoto@zozo.com>
2024-08-08 10:51:57 +0900

    poc change

--- pages/hello.jsx
+++ pages/hello.jsx
@@ -1,1 +1,23 @@
+import cookie from 'cookie';
+
+export async function getServerSideProps({ req, res }) {
+  console.log("####################################################")
+  console.log(res.getHeader('Set-Cookie'));
+  const setCookie = res.getHeader('Set-Cookie')
+  res.setHeader(
+    'Set-Cookie',
+    [
+    cookie.serialize('nextJsCookie', 'nextJsValue', {
+      path: '/',
+      httpOnly: true,
+      sameSite: 'lax',
+      maxAge: 3600,
+    }), setCookie]
+  );
+
+  return {
+    props: {},
+  };
+}
+
 export default () => <div>hello world</div>
